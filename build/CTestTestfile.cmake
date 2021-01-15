5a8e29f52ac24b8b1c28d210f44c3404292eb77d
emilia-szymanska <emiliaszym@gmail.com>
2021-01-15 01:03:00 +0100

    Remove unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emilia/turtlebot_control/src
-# Build directory: /home/emilia/turtlebot_control/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("turtlebot3_control")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/emilia/turtlebot_control/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/emilia/turtlebot_control/devel;/home/emilia/turtle_ws/devel;/opt/ros/noetic'.split(';') if '/home/emilia/turtlebot_control/devel;/home/emilia/turtle_ws/devel;/opt/ros/noetic' != '' else []
